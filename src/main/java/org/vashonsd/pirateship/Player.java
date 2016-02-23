8d20d41b97fbcd105b3f7ed583f5b8fa15fa9f42
okayThen <s.cushing98@gmail.com>
2016-02-23 09:18:23 -0800

    Update Player.java

--- src/main/java/org/vashonsd/pirateship/Player.java
+++ src/main/java/org/vashonsd/pirateship/Player.java
@@ -2,33 +2,82 @@ package org.vashonsd.pirateship;
 
 import org.vashonsd.pirateship.structure.*;
 
+import java.util.*;
 
 /**
- * @author andy
  * The Player is really the keeper of state in this system.
  */
-public class Player {
+
+public class Player 
+{
 	private String name;
-	private Location currentLocation;
+	private Hashmap<Item, int> inventory;
+	private Location current;
 	
-	public Player(String name) {
-		super();
-		this.name = name;
+	public Player(String name) 
+	{
+		this.name  = name;
+		inventory = new Hashmap<Item, int>();
+	}
+	
+	public String toString() 
+	{
+		return "Player: " + name + "is at location: " + current.getName();
+	}
+	
+	public void addItem(Item other, int quan)
+	{
+		inventory.put(other, quan);
+	}
+	
+	public void removeItem(Item other)
+	{
+		inventory.remove(other);
+	}
+	
+	public void removeItem(Item other, int quan)
+	{
+		int cur = inventory.get(other);
+		
+		inventory.remove(other);
+		inventory.put(other, quan);
+	}
+	
+	public void dumpInventory()
+	{
+		inventory.clear();
+	}
+	
+	public String inventoryToString()
+	{
+		String toReturn;
+		
+		for(Item n: inventory.keySet())
+			toReturn += "Item: " + n.getName() + "Quantity: " + inventory.get(n) + "\n";
+			
+		return toReturn;
+	}
+	
+	public Hashmap getInventory()
+	{
+		return inventory;
 	}
 
-	public String getName() {
-		return name;
+	public void changeLocation(Location next) 
+	{
+		current.leave();
+		this.current = next;
+		next.enter();
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	
+	public String locationToString()
+	{
+		return current.toString();
 	}
-
-	public Location getCurrentLocation() {
-		return currentLocation;
+	
+	public Location getLocation()
+	{
+		return current;
 	}
-
-	public void setCurrentLocation(Location currentLocation) {
-		this.currentLocation = currentLocation;
-	}
+	
 }
