5a95ac9788019bd8a59876b27dfe40dcdcfc726b
okayThen <s.cushing98@gmail.com>
2016-03-01 09:22:06 -0800

    Update ItemType.java

--- src/main/java/org/vashonsd/pirateship/ItemType.java
+++ src/main/java/org/vashonsd/pirateship/ItemType.java
@@ -1,9 +1,26 @@
 public class ItemType
 {
-    public ItemType(){}
-    
+    private ArrayList<String> tags;
     private EquipBehavior equip;
     private ValueType value;
     private EdibleBehavior edible;
+    private StatBehavior stats;
     
+    public ItemType(){
+        tags = new ArrayList<String>();
+        setTags();
+    }
+    
+    public void setTags()
+    {
+        tags.add("Rusty");
+        tags.add("Shiny");
+        tags.add("Old");
+        tags.add("New");
+        tags.add("Cursed");
+        tags.add("Blessed");
+        tags.add("Light");
+        tags.add("Heavy");
+        tags.sort();
+    }
 }
