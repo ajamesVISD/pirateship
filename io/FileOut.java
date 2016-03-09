e7182b13355ee9432cb220c8fcfe12a6227e6f1c
79305 <saschadsi@gmail.com>
2016-03-09 09:03:57 -0800

    Added files via upload
    Finished File I/O

---
+++ io/FileOut.java
@@ -0,0 +1,24 @@
+package org.vashonsd.pirateship.io;
+import java.io.*;
+
+public class FileOut implements StringWrite {
+	
+	private File file;
+	private FileWriter writer;
+	
+	public FileOut(String fileName) throws IOException {
+		file = new File(fileName);
+		writer = new FileWriter(file);
+	}
+	//FileOut requires the fileName as a string, it should equal the name of the 
+	//text file you are using to record output.
+	
+	public void write(String s) throws IOException {
+		s = s.replace("\n", "\r\n");
+		writer.write(s, 0, s.length());
+	}
+
+	public void close() throws IOException {
+		writer.close();
+	}	
+}
---
+++ io/StringRead.java
@@ -0,0 +1,19 @@
+package org.vashonsd.pirateship.io;
+
+import java.io.*;
+
+/**
+ * @author andy
+ * This is a simplified version of the built-in Java StringReader interface, which has more methods for
+ * reading and parsing and gracefully shutting down results.
+ * 
+ * Future versions may want to read Strings, or []bytes, or even whole objects. The Java interfaces can
+ * handle those, they're just more complex.
+ * 
+ * Notice this interface could be implemented by a file reader, or something that reads a stream from a
+ * network, or whatever you want.
+ */
+public interface StringRead {
+	public String read() throws IOException;
+	public void close() throws IOException;
+}
