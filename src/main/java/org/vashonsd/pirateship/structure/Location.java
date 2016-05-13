ddf1a59bef91f8c5feec8110bc9a0a47130e9647
Andy James <ajames@vashonsd.org>
2016-05-13 09:03:48 -0700

    Locations can now return their Routes and connected Locations.

--- src/main/java/org/vashonsd/pirateship/structure/Location.java
+++ src/main/java/org/vashonsd/pirateship/structure/Location.java
@@ -1,5 +1,7 @@
 package org.vashonsd.pirateship.structure;
 
+import java.util.ArrayList;
+
 import org.vashonsd.pirateship.commands.TravelCommand;
 import org.vashonsd.pirateship.interactions.Actor;
 
@@ -11,6 +13,8 @@ import org.vashonsd.pirateship.interactions.Actor;
  * It can, through the routes list, figure out what destinations it can go to.
  */
 public class Location extends Actor {
+	
+	private ArrayList<Route> routes;
 
 	public Location(String name, String description, String splash)
 	{
@@ -24,6 +28,20 @@ public class Location extends Actor {
 		Route r = new Route(accessor, description, splash, this, dest);
 		r.enrollCommand(new TravelCommand(accessor, dest));
 		addToInventory(r);
+		routes.add(r);
+	}
+	
+
+	public ArrayList<Route> getRoutes() {
+		return routes;
+	}
+	
+	public ArrayList<Location> getDestinations() {
+		ArrayList<Location> destinations = new ArrayList<Location>();
+		for (Route r : routes) {
+			destinations.add(r.getDestination());
+		}
+		return destinations;
 	}
 	
 	/**
