23095fb424bce7e46f3f85a429013d6b19ce6763
laurelgothub <laurelp16@gmail.com>
2016-06-12 21:44:10 -0700

    more cows

--- src/main/java/org/vashonsd/pirateship/creature/CreatureFactory.java
+++ src/main/java/org/vashonsd/pirateship/creature/CreatureFactory.java
@@ -15,8 +15,41 @@ public class CreatureFactory {
 		else if(which.equals("dog")) {
 			return new Dog("Jake the Dog", "He is a loyal Labrador", "A dog is wagging his tail");
 		}
-		else if(which.equals("cow")) {
-			return new Cow("Moofred", "His name is a joke, just like his life", "A cow grazes");
+		else if(which.equals("cowner")) {
+			Creature c = new Cow("Cowner", "He thinks he's pretty a-moo-sing.", "There's a cow making terrible bad jokes.");
+			return c;
+		}
+		else if (which.equals("moofred")) {
+			Creature c = new Cow("Moofred", "He was last years olympic champion mastication.", "You see a cow doing mad bench presses.");
+			return c;
+		}
+		else if (which.equals("ribecca")) {
+			Creature c = new Cow("Ribecca", "She's wearing sparkly hoofpolish.", "There's a pretty cow receptionist.");
+			return c;
+		}
+		else if (which.equals("hoofley")) {
+			Creature c = new Cow("Hoofley", "", "There's a cow sorting through mail.");
+			return c;
+		}
+		else if (which.equals("beefanca")) {
+			Creature c = new Cow("Beefanca", "She's ready to improve your shopping experience.", "A cow greets you from behind the counter.");
+			return c;
+		}
+		else if (which.equals("steakbastion")) {
+			Creature c = new Cow("Steakbastion", "He's wardrobe is rather dapper for this side of town.", "A bartender cow stands coolly at the counter.");
+			return c;
+		}
+		else if (which.equals("cudsmo")) {
+			Creature c = new Cow("Cudsmo", "He might have a beef to pick with you.", "You see a cow, hidden in the shadows.");
+			return c;
+		}
+		else if (which.equals("grazelda")) {
+			Creature c = new Cow("Grazelda", "She looks disallusioned with life.", "");
+			return c;
+		}
+		else if (which.equals("larry")) {
+			Creature c = new Cow("Larry", "This is the stuff losers are made of.", "");
+			return c;
 		}
 		else {
 			return null;
